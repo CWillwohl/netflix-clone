d306c9efada2370da20b5abc200929db71347b47
CAIO <cg3011267@LAB204M20.IFSP>
2022-09-20 21:44:22 -0300

    Aula 20/09/2022

--- src/App.js
+++ src/App.js
@@ -1,35 +1,34 @@
-import React, { useState, useEffect } from 'react'
 import './App.css'
-import Tmdb from './Tmdb'
-import Header from './components/Header'
-import FeaturedMovie from './components/FeaturedMovie'
+import React from 'react'
+import { connect } from 'react-redux'
+import { BrowserRouter } from 'react-router-dom'
+import Routes from './Routes'
+import { Template } from './components/MainComponents'
+import Header from './components/partials/Header'
+import Footer from './components/partials/Footer'
 
-export default () => {
-  const [movieList, setMovieList] = useState([]);
-  const [featuredData, setFeaturedData] = useState(null)
-  const [blackHeader, setBlackHeader] = useState(false)
-
-  useEffect(() => {
-    const loadAll = async () => {
-      let list = await Tmdb.getHomeList();
-      console.log(list)
-      setMovieList(list)
-      let originals = list.filter(i => i.slug === 'originals')
-      let randomChosen = Math.floor(Math.random() * (originals[0].items.results.length - 1))
-      let chosen = originals[0].items.results[randomChosen]
-      let chosenInfo = await Tmdb.getMovieInfo(chosen.id, 'tv')
-      console.log(chosenInfo)
-      setFeaturedData(chosenInfo)
-    }
-    loadAll()
-    }, [])
-
+const Page = (props) => {
   return (
-    <div className="page">
-        <Header black={blackHeader} />
-        {featuredData &&
-        <FeaturedMovie item={featuredData} />
-        }
-    </div>
+    <BrowserRouter>
+      <Template>
+        <Header />
+        <Routes />
+        <Footer />
+      </Template>
+    </BrowserRouter>
   )
 }
+
+const mapStateToProps = (state) => {
+  return {
+    user: state.user
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps) (Page)
